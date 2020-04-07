19005af183adff60a1822cc2f3a821a3abc55bfb
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2020-04-07 11:49:10 +0200

    Force add files

---
+++ src/main/java/com/aceql/client/metadata/dto/TableDto.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of AceQL Client SDK.
+ * AceQL Client SDK: Remote JDBC access over HTTP with AceQL HTTP.
+ * Copyright (C) 2020,  KawanSoft SAS
+ * (http://www.kawansoft.com). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.aceql.client.metadata.dto;
+
+import com.aceql.client.metadata.Table;
+
+/**
+ * Contains the list of tables of the database.
+ * @author Nicolas de Pomereu
+ *
+ */
+public class TableDto {
+
+    private String status = "OK";
+    private Table table = null;
+
+    public TableDto(Table table) {
+	super();
+	this.table = table;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Table getTable() {
+        return table;
+    }
+
+    @Override
+    public String toString() {
+	return "TableDto [status=" + status + ", table=" + table + "]";
+    }
+
+}
