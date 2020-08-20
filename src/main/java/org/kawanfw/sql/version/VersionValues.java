42e5c74e80243e927cbe1715dd1cfb682b837657
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2020-08-20 20:11:56 +0200

    Update version

--- src/main/java/org/kawanfw/sql/version/VersionValues.java
+++ src/main/java/org/kawanfw/sql/version/VersionValues.java
@@ -23,6 +23,6 @@ package org.kawanfw.sql.version;
  */
 
 public class VersionValues {
-    public static final String VERSION = "v5.0";
+    public static final String VERSION = "v4.3";
     public static final String DATE = "20-Aug-2020";
 }
