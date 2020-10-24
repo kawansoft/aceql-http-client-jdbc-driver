da99f3bab74c5bb994dbc01d6c1ae3fc2d0c07f3
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2020-10-24 13:00:03 +0200

    version 5.0.2

---
+++ src/test/java/com/aceql/client/test/TestAllNew.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.aceql.client.test;
+
+/**
+ * @author Nicolas de Pomereu
+ *
+ */
+public class TestAllNew {
+
+    /**
+     * 
+     */
+    public TestAllNew() {
+	// TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
