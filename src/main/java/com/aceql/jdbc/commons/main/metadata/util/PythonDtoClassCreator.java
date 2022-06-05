12f91aa53b75ad95c310566aeb8b4bd4c5317228
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-06-05 19:00:22 +0200

    PythonDtoClassCreator enhance & clean

--- src/main/java/com/aceql/jdbc/commons/main/metadata/util/PythonDtoClassCreator.java
+++ src/main/java/com/aceql/jdbc/commons/main/metadata/util/PythonDtoClassCreator.java
@@ -1,9 +1,26 @@
-/**
+/*
+ * This file is part of AceQL JDBC Driver.
+ * AceQL JDBC Driver: Remote JDBC access over HTTP with AceQL HTTP.
+ * Copyright (C) 2021,  KawanSoft SAS
+ * (http://www.kawansoft.com). All rights reserved.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package com.aceql.jdbc.commons.main.metadata.util;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -19,16 +36,17 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 
-import com.aceql.jdbc.commons.metadata.Index;
+import com.aceql.jdbc.commons.metadata.Column;
 
 /**
+ * A Translator of Java DTP to Python DTO.
+ * 
  * @author Nicolas de Pomereu
  *
  */
-public class PythonClassDumper {
+public class PythonDtoClassCreator {
 
-    private static final String PYTHON_HEADERS_FILE = 
-	    "I:\\_dev_awake\\aceql-http-main\\aceql-http-client-jdbc-driver\\src\\main\\java\\com\\aceql\\jdbc\\commons\\main\\metadata\\util\\python_header.txt";
+    private static final String PYTHON_HEADERS_FILE = "I:\\_dev_awake\\aceql-http-main\\aceql-http-client-jdbc-driver\\src\\main\\java\\com\\aceql\\jdbc\\commons\\main\\metadata\\util\\python_header.txt";
     private static final String FOUR_BLANKS = "    ";
 
     /**
@@ -40,52 +58,75 @@ public class PythonClassDumper {
 
 	List<Class<?>> classes = new ArrayList<Class<?>>();
 	/*
+	 * classes.add(Column.class); 
+	 * classes.add(ExportedKey.class);
+	 * classes.add(ForeignKey.class); 
+	 * classes.add(ImportedKey.class);
+	 * classes.add(Index.class); 
+	 * classes.add(PrimaryKey.class);
+	 * classes.add(DatabaseInfoDto.class);
+	 */
 	classes.add(Column.class);
-	classes.add(ExportedKey.class);
-	classes.add(ForeignKey.class);
-	classes.add(ImportedKey.class);
-	classes.add(Index.class);
-	classes.add(PrimaryKey.class);
-	*/
-	classes.add(Index.class);
-	//classes.add(DatabaseInfoDto.class);
-	
-	File baseDir = new File(SystemUtils.USER_HOME + File.separator + "tmp");
-	baseDir.mkdirs();
-	
-	for (Class<?> clazz : classes) {
-	    //System.out.println(new Date() + " " + clazz.getSimpleName());
-	    String pyfileName = baseDir + File.separator + clazz.getSimpleName().toLowerCase() + ".py";
-	    System.out.println(new Date() + " Python File created: " + pyfileName);
-	    try (PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(pyfileName)));) {
-		printPythonClass(clazz, printHeader, out);
-	    }
 
-	    System.out.println();
-	    //java.awt.Desktop.getDesktop().edit(new File(pyfileName));
+	PythonDtoClassCreator pythonDtoClassCreator = new PythonDtoClassCreator();
+	pythonDtoClassCreator.generatePythonClasses(printHeader, classes);
 
-	}
-	//System.out.println("Generated: " + classes);
-	System.out.println(new Date() + " Done.");
     }
 
     /**
-     * @param clazz
-     * @param printHeader
-     * @param out        
+     * Generate Python files from a List of Java classes translated to Python
+     * 
+     * @param includeHeader if true, include the header PYTHON_HEADERS_FILE
+     * @param classes       the Java classes list to r
+     * @throws SecurityException
+     * @throws IOException
+     * @throws FileNotFoundException
+     */
+    public void generatePythonClasses(boolean includeHeader, List<Class<?>> classes)
+	    throws SecurityException, IOException, FileNotFoundException {
+	File baseDir = new File(SystemUtils.USER_HOME + File.separator + "tmp");
+	baseDir.mkdirs();
+
+	for (Class<?> clazz : classes) {
+	    String timestamp = PythonDtoClassUtil.getTimestamp();
+	    String pyfileName = baseDir + File.separator + clazz.getSimpleName().toLowerCase() + ".py";
+	    System.out.println(
+		    timestamp + " Python File created from Java class " + clazz.getSimpleName() + ".java : " + pyfileName);
+	    try (PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(pyfileName)));) {
+		createPythonFile(clazz, includeHeader, out);
+	    }
+
+	    Runtime runtime = Runtime.getRuntime();
+	    runtime.exec(new String[] { "C:\\Program Files (x86)\\Notepad++\\notepad++.exe", pyfileName });
+
+	}
+
+	String timestamp = PythonDtoClassUtil.getTimestamp();
+	System.out.println(timestamp + " Done.");
+    }
+
+
+    /**
+     * Create a Python file translarted from a Java class
+     * 
+     * @param clazz         the Java class to translate
+     * @param includeHeader if true, include the header PYTHON_HEADERS_FILE
+     * @param out           the write destination
      * @throws SecurityException
      * @throws IOException
      */
-    private static void printPythonClass(Class<?> clazz, boolean printHeader, PrintWriter out)
+    private void createPythonFile(Class<?> clazz, boolean printHeader, PrintWriter out)
 	    throws SecurityException, IOException {
 	Field[] fields = clazz.getDeclaredFields();
 
 	String header = FileUtils.readFileToString(new File(PYTHON_HEADERS_FILE), Charset.defaultCharset());
 	out.println(header);
+
+	String timestamp = PythonDtoClassUtil.getTimestamp();
+
 	/**
 	 * from dataclasses import dataclass import marshmallow_dataclass
-	 *
-	 *
+	 * 
 	 * @dataclass class JdbcDatabaseMetaData:
 	 */
 	out.println("from dataclasses import dataclass");
@@ -95,6 +136,7 @@ public class PythonClassDumper {
 	out.println();
 	out.println("@dataclass");
 	out.println("class " + clazz.getSimpleName() + ":");
+	out.println("    \"\"\" Generated on " + timestamp + " from " + clazz.getSimpleName() + ".java  translation\"\"\" ");
 
 	/*
 	 * Header getURL: str isReadOnly: bool allProceduresAreCallable: bool
@@ -145,19 +187,15 @@ public class PythonClassDumper {
 
 	String stringValue = FOUR_BLANKS + FOUR_BLANKS + "return " + "\"" + clazz.getSimpleName() + " [";
 
-	// Primary Keys : [PrimaryKey [tableName=user_login, columnName=username,
-	// keySequence=1, primaryKeyName=user_login_pkey,
-	// getCatalog()=, getSchema()=public]]
-
 	for (String name : names) {
 	    stringValue += name + "=\" + str(self." + name + ")" + " + \", ";
 	}
 
-	// stringValue = StringUtils.substringBeforeLast(stringValue, "+");
 	stringValue = StringUtils.substringBeforeLast(stringValue, "+");
 	stringValue += " + \"]\"";
 
 	out.println(stringValue);
     }
+
 
 }
