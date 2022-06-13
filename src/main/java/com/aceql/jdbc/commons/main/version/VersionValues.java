e2c8ac532a8f9d933d65227f4ab8f475337486f3
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-06-13 16:00:40 +0200

    Upcate version date

--- src/main/java/com/aceql/jdbc/commons/main/version/VersionValues.java
+++ src/main/java/com/aceql/jdbc/commons/main/version/VersionValues.java
@@ -24,5 +24,5 @@ package com.aceql.jdbc.commons.main.version;
 
 public class VersionValues {
     public static final String VERSION = "v9.1";
-    public static final String DATE = "11-Jun-2022";
+    public static final String DATE = "13-Jun-2022";
 }
